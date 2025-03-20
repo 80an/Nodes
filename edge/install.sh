74c35a66d8388d1ad030e34cafd3b8db20323a48
80Humor <102892204+80an@users.noreply.github.com>
2025-03-20 09:33:12 +0300

    Update install.sh

--- edge/install.sh
+++ edge/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Подготовка сервера
+source <(wget -qO- 'https://raw.githubusercontent.com/80an/Nodes/refs/heads/main/!tools/server_prepare.sh')
+
+
 # Скрипт для установки и запуска ноды LayerEdge Light Node
 
 set -e  # Остановка при ошибке
@@ -33,6 +37,7 @@ then
 fi
 
 echo "Rust установлен: $(rustc --version)"
+echo "Rust установлен: $(rustup --version)"
 
 # Установка Risc0 Toolchain
 if ! command -v rzup &> /dev/null
@@ -43,6 +48,7 @@ fi
 
 echo "Risc0 Toolchain установлен."
 
+# Установка ноды
 # 2. Клонирование репозитория
 if [ ! -d "light-node" ]; then
     echo "Клонируем репозиторий..."
@@ -53,28 +59,36 @@ cd light-node
 echo "Репозиторий клонирован."
 
 # 3. Настройка переменных окружения
+echo -n "Введите PRIVATE_KEY: "
+read -s PRIVATE_KEY
+export PRIVATE_KEY
+export GRPC_URL=34.31.74.109:9090
+export CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
+export ZK_PROVER_URL=http://127.0.0.1:3001
+export API_REQUEST_TIMEOUT=100
+export POINTS_API=http://127.0.0.1:8080
+
 cat > .env <<EOL
 GRPC_URL=34.31.74.109:9090
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
-# Альтернативный URL
-ZK_PROVER_URL=https://layeredge.mintair.xyz/
 API_REQUEST_TIMEOUT=100
-POINTS_API=https://light-node.layeredge.io
-PRIVATE_KEY='cli-node-private-key'
+POINTS_API=http://127.0.0.1:8080
+PRIVATE_KEY='$PRIVATE_KEY'
 EOL
 
 echo "Переменные окружения настроены."
 
-# 4. Запуск Merkle-сервиса
+# 4. Запуск Merkle-сервиса в screen-сессии
 cd risc0-merkle-service
-cargo build && cargo run &
+screen -S layeredge_server -dm bash -c "cargo build && cargo run"
 cd ..
 
-echo "Merkle-сервис запущен."
+echo "Merkle-сервис запущен в screen-сессии. Для возврата: screen -r layeredge_server"
 
 # 5. Компиляция и запуск Light Node
+sleep 5  # Ожидание инициализации Merkle-сервиса
 go build
-./light-node &
+./light-node
 
 echo "LayerEdge Light Node запущена!"
