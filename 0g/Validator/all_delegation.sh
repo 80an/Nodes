56efc313566aae6fbcc6c980dc9fffc9ce8839a5
80Humor <102892204+80an@users.noreply.github.com>
2025-04-13 07:31:22 +0300

    Update and rename 0g/all_delegation.sh to 0g/Validator/all_delegation.sh

--- 0g/Validator/all_delegation.sh
+++ 0g/Validator/all_delegation.sh
@@ -1,35 +1,18 @@
 #!/bin/bash
 
-ENV_FILE="$HOME/.validator_env"
-
-# Проверяем наличие .env файла
-if [ ! -f "$ENV_FILE" ]; then
-  echo "❌ Файл .env ($ENV_FILE) не найден. Пожалуйста, сначала настройте валидатора."
-  exit 1
-fi
-
-# Загружаем переменные
-source "$ENV_FILE"
-
-# Проверка необходимых переменных
-if [ -z "$KEYRING_PASSWORD" ] || [ -z "$VALIDATOR_ADDRESS" ]; then
-  echo "❌ Не найдены необходимые переменные: KEYRING_PASSWORD или VALIDATOR_ADDRESS"
-  exit 1
-fi
-
 # Получаем список кошельков
 wallet_names=$(printf "%s" "$KEYRING_PASSWORD" | 0gchaind keys list | grep "name:" | awk '{print $2}')
 
 # Обрабатываем каждый кошелек
-for wallet_name in $wallet_names
+for WALLET_NAME in $wallet_names
 do
-    balance_info=$(printf "%s" "$KEYRING_PASSWORD" | 0gchaind q bank balances $(0gchaind keys show "$wallet_name" -a))
+    balance_info=$(printf "%s" "$KEYRING_PASSWORD" | 0gchaind q bank balances $(0gchaind keys show "$WALLET_NAME" -a))
     amount=$(echo "$balance_info" | grep -B 1 "ua0gi" | grep "amount" | awk '{print $2}' | tr -d '"')
 
     if [ -n "$amount" ] && [ "$amount" -ge 100 ] 2>/dev/null; then
         echo "🪙 Кошелек: $wallet_name, Баланс: ${amount} ua0gi"
         printf "%s" "$KEYRING_PASSWORD" | 0gchaind tx staking delegate "$VALIDATOR_ADDRESS" "${amount}ua0gi" \
-          --from "$wallet_name" \
+          --from "$WALLET_NAME" \
           --gas=auto \
           --gas-prices 0.003ua0gi \
           --gas-adjustment=1.4 \
