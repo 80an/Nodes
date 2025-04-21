596767901b5347d58d107c9b7a807d17848338e2
80Humor <102892204+80an@users.noreply.github.com>
2025-04-21 16:34:12 +0300

    Update all_reward.sh

--- 0g/Validator/all_reward.sh
+++ 0g/Validator/all_reward.sh
@@ -7,13 +7,13 @@ if [ -z "$KEYRING_PASSWORD" ] || [ -z "$VALIDATOR_ADDRESS" ]; then
 fi
 
 # Получаем список имён кошельков
-wallet_names=$(printf '%s' "$KEYRING_PASSWORD" | 0gchaind keys list | grep "name:" | awk '{print $2}')
+wallet_names=$(echo "$KEYRING_PASSWORD" | 0gchaind keys list | grep "name:" | awk '{print $2}')
 
 # Проходим по каждому кошельку
 for wallet_name in $wallet_names; do
   echo "💰 Выводим реварды для $wallet_name"
 
-  printf '%s' "$KEYRING_PASSWORD" | 0gchaind tx distribution withdraw-rewards "$VALIDATOR_ADDRESS" \
+  echo "$KEYRING_PASSWORD" | 0gchaind tx distribution withdraw-rewards "$VALIDATOR_ADDRESS" \
     --chain-id="zgtendermint_16600-2" \
     --from "$wallet_name" \
     --gas=auto \
