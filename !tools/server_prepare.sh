c645d410524a3edc46bbcf3ba44297702c558dae
80Humor <102892204+80an@users.noreply.github.com>
2025-04-22 13:55:21 +0300

    Update server_prepare.sh

--- !tools/server_prepare.sh
+++ !tools/server_prepare.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Import Colors
+source <(wget -qO- 'https://raw.githubusercontent.com/CBzeek/Nodes/refs/heads/main/!tools/bash-colors.sh')
+
 cd $HOME
 
 #Ubuntu update and upgrade
-print_header "Updating and Upgrading server..."
+print_header "Обновляем и подготавливаем сервер к работе..."
 sudo apt update && sudo apt upgrade -y
 
 
 #Install software
-print_header "Installing dependencies to server..."
+print_header "Устанавливаем необходимые пакеты на сервер..."
 sudo apt install curl mc git jq screen lz4 build-essential htop zip unzip wget rsync snapd -y
 sudo snap install yq
