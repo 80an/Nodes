d978c1698fbf7452a7c66541001eff530ba910b5
80Humor <102892204+80an@users.noreply.github.com>
2025-07-03 15:12:21 +0300

    Update upgrade.sh

--- nexus/upgrade.sh
+++ nexus/upgrade.sh
@@ -16,10 +16,7 @@ docker rm nexus
 docker pull nexusxyz/nexus-cli:latest
 
 # Устанавливаем screen, если он еще не установлен
-sudo apt install screen -y
-
-# Запускаем новую сессию screen и выполняем команду
-screen -dmS nexus docker run -it --init --name nexus nexusxyz/nexus-cli:latest start --node-id "$NODE_ID"
+sudo apt install screen -y && screen -Rd nexus
 
 # Сообщаем пользователю, что сессия запущена
 echo "Сессия screen 'nexus' запущена. Для подключения используйте: screen -Rd nexus"
